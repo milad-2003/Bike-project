0ed5e30de7eee5df269f6e93c050dd5e4c45e994
milad-2003 <miladvalizadeh2003@gmail.com>
2023-12-13 13:23:58 +0330

    Adding the "User" class.

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+class User:
+
+    def __init__(self, f_name, l_name, username, rental_list = []):
+        self.f_name = f_name
+        self.l_name = l_name
+        self.username = username
+        self.rental_list = rental_list
+
+
 def main():
     pass
 
